faee7d96c61462ca1370a69e6ca45ba42b89db06
Luis Fernando <luis.silva9902@gmail.com>
2022-03-11 07:10:09 -0300

    add background, new features and fixed bugs

--- script.js
+++ script.js
@@ -1,67 +1,97 @@
 let numbers = []
 let currentNumber = ''
 let lastOperation = ''
+let historyCalc = ''
 let elementDisplayResult = document.querySelector('.element-display-result')
 let elementDisplayCalc = document.querySelector('.element-display-calc')
+let buttonClear = document.getElementById('clear')
+
+
+function clearCalculator() {
+  buttonClear.textContent = 'AC'
+  currentNumber = ''
+  numbers = []
+  historyCalc = ''
+  elementDisplayCalc.textContent = ''
+  elementDisplayResult.textContent = '0'
+  elementDisplayCalc.classList.remove("element-display-calc-total")
+  elementDisplayResult.classList.remove("element-display-result-total")
+}
 
 function calculate(element) {
 
-  if(!isNaN(element) && numbers.length === 0){
+  if (!isNaN(element) && numbers.length === 0) {
+    elementDisplayCalc.classList.remove("element-display-calc-total")
+    elementDisplayResult.classList.remove("element-display-result-total")
+    buttonClear.textContent = 'C'
     currentNumber += element
-  } else if (isNaN(element) && numbers.length === 0) {
+    historyCalc = currentNumber
+  } else if (isNaN(element) && currentNumber.length === 0) {
+    return
+  } else if (isNaN(element) && numbers.length === 0 && element !== "=") {
     numbers[0] = Number(currentNumber)
     currentNumber = ''
+    historyCalc += element
     lastOperation = element
   }
 
-  if(isNaN(element) && numbers.length > 0){
-    if(element === "=" && numbers[1] !== undefined){
+  if (isNaN(element) && numbers.length > 0) {
+    if (element === "=" && numbers[1] !== undefined) {
       elementDisplayResult.textContent = "= " + numbers[1]
       currentNumber = ''
-      elementDisplayCalc.textContent = currentNumber
+      elementDisplayCalc.classList.add("element-display-calc-total")
+      elementDisplayResult.classList.add("element-display-result-total")
       numbers = []
       return
     }
-    if(lastOperation !== element){
+
+
+
+    if (lastOperation !== element) {
       lastOperation = element
       numbers[0] = numbers[1]
       currentNumber = ''
-    } else if(lastOperation === element && numbers[1] !== undefined) {
+      historyCalc += element
+    } else if (lastOperation === element && numbers[1] !== undefined) {
       numbers[0] = numbers[1]
       currentNumber = ''
+      historyCalc += element
     }
-  } 
+  }
 
-  if(!isNaN(element) && numbers.length > 0){
-    switch(lastOperation){
-      case'+':
+  if (!isNaN(element) && numbers.length > 0) {
+    historyCalc += element
+    switch (lastOperation) {
+      case '+':
         currentNumber += element
         numbers[1] = numbers[0] + Number(currentNumber)
         break
-      case'-':
+      case '-':
         currentNumber += element
         numbers[1] = numbers[0] - Number(currentNumber)
         break
-      case'*':
+      case '*':
         currentNumber += element
         numbers[1] = numbers[0] * Number(currentNumber)
         break
-      case'/':
+      case '/':
         currentNumber += element
-        numbers[1] =  (numbers[0] / Number(currentNumber)).toFixed(7)
+        numbers[1] = (numbers[0] / Number(currentNumber)).toLocaleString('en-US', {
+          minimumFractionDigits: 2,
+          maximumFractionDigits: 7
+        })
         break
     }
   }
-  
-  elementDisplayCalc.textContent = currentNumber
 
-  if(numbers[1] || numbers[1] === 0){
+  if (numbers[1] || numbers[1] === 0) {
     elementDisplayResult.textContent = "= " + numbers[1]
-  } else if(numbers[0]){
+  } else if (numbers[0]) {
     elementDisplayResult.textContent = "= " + numbers[0]
-  } else if(!isNaN(currentNumber)){
+  } else if (!isNaN(currentNumber)) {
     elementDisplayResult.textContent = "= " + currentNumber
   }
 
-   
+  elementDisplayCalc.textContent = historyCalc
+
 }
